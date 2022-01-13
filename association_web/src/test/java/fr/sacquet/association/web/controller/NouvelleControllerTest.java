55924ed51d387635f300b234005585ea127819b9
DUCLOS Maxime <mduclos@sodifrance.fr>
2022-01-13 08:39:00 +0100

    Avancement des tu

--- association_web/src/test/java/fr/sacquet/association/web/controller/NouvelleControllerTest.java
+++ association_web/src/test/java/fr/sacquet/association/web/controller/NouvelleControllerTest.java
@@ -1,14 +1,17 @@
 package fr.sacquet.association.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.sacquet.association.web.bean.Nouvelle;
 import fr.sacquet.association.web.model.NouvelleRequest;
 import fr.sacquet.association.web.services.NouvelleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.google.common.net.HttpHeaders;
 import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 
 import java.io.FileInputStream;
@@ -18,11 +21,15 @@ import java.util.List;
 import static fr.sacquet.association.web.conf.Constante.PRIVATE_API;
 import static fr.sacquet.association.web.conf.Constante.PUBLIC_API;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class NouvelleControllerTest extends AbstractControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class NouvelleControllerTest {
 
     @Autowired
     private MockMvc mvc;
@@ -68,7 +75,7 @@ class NouvelleControllerTest extends AbstractControllerTest {
         when(service.createNouvelle(new NouvelleRequest("", ""))).thenReturn(news);
 
         // When and Then
-        this.mvc.perform(post(PRIVATE_API + "/nouvelles")
+        this.mvc.perform(post(PRIVATE_API + "/nouvelle")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
     }
@@ -79,16 +86,20 @@ class NouvelleControllerTest extends AbstractControllerTest {
         FileInputStream fis = new FileInputStream(BASE_PATH + "nouvelleReturn.json");
         String data = IOUtils.toString(fis, "UTF-8");
         Nouvelle news = new Nouvelle();
-        news.setDescription("description");
-        news.setTitre("title");
+        news.setDescription("description1");
+        news.setTitre("title1");
         news.setId(1);
+        NouvelleRequest nr = new NouvelleRequest("title1", "description1");
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(nr);
 
         // Given
         when(service.createNouvelle(new NouvelleRequest("", ""))).thenReturn(news);
 
         // When and Then
-        this.mvc.perform(post(PRIVATE_API + "/nouvelles")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + authToken)
+        this.mvc.perform(post(PRIVATE_API + "/nouvelle")
+                        .with(jwt())
+                        .content(jsonString)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(data.trim()));
@@ -105,7 +116,7 @@ class NouvelleControllerTest extends AbstractControllerTest {
     void deleteNouvelle_useCase() throws Exception {
         // When and Then
         this.mvc.perform(delete(PRIVATE_API + "/nouvelle/1")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + authToken))
-                .andExpect(status().isUnauthorized());
+                        .with(jwt()))
+                .andExpect(status().isOk());
     }
 }
