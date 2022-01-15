99f03fc8f1468e57cc9b4712d01d6e1d7acf9599
DUCLOS Maxime <mduclos@sodifrance.fr>
2022-01-15 15:27:36 +0100

    Correction Angular sur le choix du menu de navigation.

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {AdresseService} from "./services/adresse.service";
-import {Component, OnInit} from "@angular/core";
-import {MenuItem} from "primeng/api";
-import {AuthenticationService} from "./authentication/authentication.service";
+import {AdresseService} from './services/adresse.service';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
+import {AuthenticationService} from './authentication/authentication.service';
 
 @Component({
     selector: 'app-root',
@@ -12,9 +12,9 @@ export class AppComponent implements OnInit {
 
     tabMenuItems: MenuItem[];
     tabMenuItemsBase = [
-        {label: '', icon: 'pi pi-home', routerLink: ['home']},
-        {label: 'Associations avant 2009', icon: 'pi pi-list', routerLink: ['association']},
-        {label: 'Associations après 2009', icon: 'pi pi-list', routerLink: ['association_waldec']},
+        {label: '', icon: 'pi pi-home', routerLink: 'home'},
+        {label: 'Associations avant 2009', icon: 'pi pi-list', routerLink: 'association'},
+        {label: 'Associations après 2009', icon: 'pi pi-list', routerLink: 'association_waldec'},
     ];
 
     constructor(private authService: AuthenticationService) {
@@ -30,9 +30,9 @@ export class AppComponent implements OnInit {
         if (tokenRole.Roles) {
             const roles: string[] = tokenRole.Roles.map(r => r.authority);
             if (roles.includes('ROLE_SUPER_ADMIN')) {
-                this.tabMenuItems.push({label: 'Administration', icon: 'pi pi-cog', routerLink: ['administration']});
+                this.tabMenuItems.push({label: 'Administration', icon: 'pi pi-cog', routerLink: 'administration'});
             } else if (roles.includes('ROLE_USER_CLASSIC')) {
-                this.tabMenuItems.push({label: 'Statistique', icon: 'pi pi-chart-bar', routerLink: ['statistique']});
+                this.tabMenuItems.push({label: 'Statistique', icon: 'pi pi-chart-bar', routerLink: 'statistique'});
             }
         }
     }
